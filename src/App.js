def1254626b7e88aa05780dceca3b8a45e4c5ebc
Juan Jose Sebastian Fuertes <jjsebastianfuertes@gmail.com>
2019-11-13 12:06:29 -0500

    Add: home component
    --Add:
    style.module file
    js file
    @[Jjsebastianfuertes]
    --Change:
    header style to adapt home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
-import Header from "./header/Header";
-import Footer from "./footer/Footer";
-import "./App.css";
+import React from 'react';
+import Header from './header/Header';
+import Home from './home/home';
+import Footer from './footer/Footer';
+import './App.css';
 
 function App() {
   return (
     <div>
       <Header></Header>
-
+      <Home></Home>
       <Footer></Footer>
     </div>
   );
---
+++ src/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './home.module.css';
+const home = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.item}>
+        Making brands fun. <br /> Down to a science.
+      </h1>
+    </div>
+  );
+};
+
+export default home;
